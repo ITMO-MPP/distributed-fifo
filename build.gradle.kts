73736422f22dff240526f4f95ac0208c6185c14a
Roman Elizarov <elizarov@gmail.com>
2025-03-04 23:08:23 +0300

    PDS 2025 version Java 21 Kotlin 2.1.10 Gradle 8.12.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.*
-
-group = "ru.ifmo.pds"
-version = "1.0-SNAPSHOT"
-
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "2.1.10"
+    java
     application
 }
 
@@ -15,28 +11,18 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
     implementation("ch.qos.logback:logback-classic:1.2.10")
     testImplementation(kotlin("test-junit"))
 }
 
-sourceSets {
-    main {
-        java.setSrcDirs(listOf("src"))
-    }
-    test {
-        java.setSrcDirs(listOf("test"))
-    }
+sourceSets.main {
+    java.srcDir("src")
+}
+
+sourceSets.test {
+    java.srcDir("test")
 }
 
 kotlin {
-    jvmToolchain {
-        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(11))
-    }
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
-    }
+    jvmToolchain(21)
 }
